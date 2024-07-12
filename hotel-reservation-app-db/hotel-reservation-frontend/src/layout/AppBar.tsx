dc2f04a254bc13974804d5412f5c56caaf1394a9
Sandeep Ramanayakanahalli Nagaraj <Sandeep.RN@symphonyretailai.com>
2024-07-12 17:45:40 +0530

    Implement authentication functionality for the web application

--- hotel-reservation-app-db/hotel-reservation-frontend/src/layout/AppBar.tsx
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/layout/AppBar.tsx
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
@@ -48,9 +49,14 @@ function UserMenu() {
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
-        <MenuItem onClick={() => (window.location.pathname = "/reservations")}>
-          <Button style={{ textTransform: "none" }}>
+        <MenuItem onClick={() => {
+                sessionStorage.removeItem("userInfo");
+              window.location.href =
+`/auth/logout?session_hint=${Cookies.get('session_hint')}`;
+          }}
+>
             <Typography textAlign="center">My Reservations</Typography>
+            
           </Button>
         </MenuItem>
         <MenuItem>
